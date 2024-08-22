a11d5a7738b1931fa49c5720fc9a9124fb44eff8
shegerbootcamp <shegerbootcamp@gmail.com>
2024-08-22 19:48:56 -0400

    docker compose

---
+++ README.md
@@ -0,0 +1,21 @@
+# docker-example
+
+git clone <>
+
+cd docker-example/
+
+docker-compose build
+
+docker-compose up
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ node/index.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    http = require('http'),
+    redis = require('redis');
+
+var app = express();
+
+console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
+
+var client = redis.createClient('6379', 'redis');
+
+app.get('/', function(req, res, next) {
+  client.incr('counter', function(err, counter) {
+    if(err) return next(err);
+    var ip = process.env.MY_NAME;
+    res.send('This page has been viewed ' + counter + ' times!<br><br>Request processed by ' + ip + '.');
+  });
+});
+
+http.createServer(app).listen(process.env.PORT || 8080, function() {
+  console.log('Listening on port ' + (process.env.PORT || 8080));
+});
